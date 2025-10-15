51279190e796339819bd7282615de33fdcbd6d65
Noogear <123232885+Noogear@users.noreply.github.com>
2025-10-15 22:20:10 +0800

    Add files via upload

--- src/main/java/cn/simplevaultreset/MainCommand.java
+++ src/main/java/cn/simplevaultreset/MainCommand.java
@@ -9,7 +9,6 @@ import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,7 +23,7 @@ public class MainCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+        if (args.length > 0 && "reload".equalsIgnoreCase(args[0])) {
             if (!sender.hasPermission("simplevaultreset.reload")) {
                 sender.sendMessage(Component.text("You do not have permission to use this command.", NamedTextColor.RED));
                 return true;
@@ -41,7 +40,7 @@ public class MainCommand implements CommandExecutor, TabCompleter {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
         if (args.length == 1) {
-            final List<String> completions = Arrays.asList("reload");
+            final List<String> completions = List.of("reload");
             return completions.stream()
                     .filter(str -> str.toLowerCase().startsWith(args[0].toLowerCase()))
                     .collect(Collectors.toList());
